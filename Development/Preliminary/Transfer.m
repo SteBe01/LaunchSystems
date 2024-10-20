441512fdac26919851610fe73e46d48d391f6701
Leo777Deo <goldenexp.requiem@gmail.com>
2024-10-20 13:15:12 +0200

    Delta V: id + losses e astroconstants

--- Development/Preliminary/Transfer.m
+++ Development/Preliminary/Transfer.m
@@ -5,27 +5,64 @@ close all
 
 % data
 
-mu = 398600;
+h_airplane = 10.668e3; % [m] LauncherOne service guide august 2020
 
-h_airplane = 10;            % [km]
-v_airplane = 800/3600;      % [km/s]
-h_final_orbit = 250;        % [km]
-Earth_radius = 6371;        % [km]
+M_carrier = 0.62; % [-]  LauncherOne service guide august 2020
 
-r_airplane = Earth_radius + h_airplane;
-r_final_orbit = Earth_radius + h_final_orbit;
+T_hcarr = 216.65; % [K] Wikepida temp at that h_airplane
+
+gamma = 1.4; % [-] gamma air
+
+R = 287; % [J/kg K] R of air
+
+h_f = 400e3; % [m] orbit altitude by Maggi
+
+c0 = sqrt(gamma*R*T_hcarr); % [m/s] speed of sound
+v0 = M_carrier*c0; % [m/s] speed of airplane
+
+vf = sqrt((astroConstants(13)*1000^3)/(h_f + (astroConstants(23)* 10^3 ))); % [m/s] orbital speed
+
+% Transfer orbit:
+
+ra = h_f + (astroConstants(23)* 10^3); % m
+
+rp = h_airplane + (astroConstants(23)* 10^3); % m
+
+e = (ra - rp)/(ra + rp); % -
+a = (ra +rp)/2; % [m]
+p = a*(1-e^2); % [m]
+
+vp = (sqrt((astroConstants(13)*1000^3)/p))*(1 + e); % [m/s]
+va = (sqrt((astroConstants(13)*1000^3)/p))*(1 - e);  % [m/s]
+
+DV1 = vp - v0;
+DV2 = vf - va;
+
+Delta_V_id = DV1 + DV2; % [m/s] Tsiolkovsky Delta V
+
+% Losses: 
+
+Delta_V_g = (1500+2000)/2; % m/s from Space mission analysis design, also confirmed by A study of air launch methods for RLVs
+
+Delta_V_d = 0.03 * Delta_V_id;% m/s from Space mission analysis design about 3% of total budget
+
+Delta_V_s = (30.48+182.88)/2; % m/s from A study of air launch methods for RLVs btw 100 300 fps
+
+% Total: 
+
+Delta_V_tot = Delta_V_id + Delta_V_g + Delta_V_d + Delta_V_s;
 
 % implement first orbit, r and v of first orbit
 
-rr = [r_airplane 0 0];
-vv = [0 v_airplane 0];
-v = v_airplane;
-r = r_airplane;
+rr = [rp*10^-3 0 0];
+vv = [0 v0*10^-3 0];
+v = v0;
+r = rp;
 
-E = v^2/2 - mu/r;
-a1 = -mu/(2*E);
+E = v^2/2 - astroConstants(13)/r;
+a1 = -astroConstants(13)/(2*E);
 h1 = cross(rr,vv);
-ee = cross(vv, h1)/mu - rr/r;
+ee = cross(vv, h1)/astroConstants(13) - rr/r;
 e1 = norm(ee);
 
 % implement final orbit, r and v of final orbit
