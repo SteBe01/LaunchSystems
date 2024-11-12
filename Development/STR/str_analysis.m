54ab24e9ef4cc22a0a33cbd7f65a458d1e698da3
PietroBolsi <pitero.bolsi@gmail.com>
2024-11-12 23:37:35 +0100

    Update str_analysis.m
    modified function (different tank shapes implemented)

--- Development/STR/str_analysis.m
+++ Development/STR/str_analysis.m
@@ -8,7 +8,7 @@ Is = [290; 300]; %[s] stages Is
 eps = [0.07; 0.12]; %[-] stages structural mass indexes
 dv = 8.5; %[m/s] required dv;
 m_pay = 250; %[kg] payload mass
-m_fairing = 80; %[kg] fairing mass
+m_fairing = 107; %[kg] fairing mass
 
 %GLOM from baseline
 [GLOM.m_stag, GLOM.m_tot, GLOM.m_prop] = TANDEM(Is, eps, dv, m_pay, 0);
@@ -174,22 +174,49 @@ Kp = 1.5;
 MDP = MEOP * Km * Kp;
 jproof = 1.25;
 jburst = 1.5;
-p = MDP * jburst;
+p = MDP * jproof;
 
 %tanks shape definition
 e = sqrt( 1 - 1 / AR^2 );         %eccentricity of oblate part [-]
 R_int = @(t) (diam - 2*t) / 2;    %internal radius of the tank [m]
 V_obl = @(t) (4/3)*pi*R_int(t)^3 / AR; %volume of the two oblate parts [m^3]
-V_cyl_lox = @(t) vlox - V_obl(t); %volume of the cylindrical part [m^3]
-V_cyl_rp1 = @(t) vrp1 - V_obl(t); %volume of the cylindrical part [m^3]
-h_cyl_lox = @(t) V_cyl_lox(t) / (pi*R_int(t)^2); %height of cylindrical part [m]
-h_cyl_rp1 = @(t) V_cyl_rp1(t) / (pi*R_int(t)^2); %height of cylindrical part [m]
-l_lox = @(t) h_cyl_lox(t) + 2*R_int(t)/AR + 2*t; %height of the tank [m]
-l_rp1 = @(t) h_cyl_rp1(t) + 2*R_int(t)/AR + 2*t; %height of the tank [m]
+
+%spherical tank hypothesis
+R_sphere_lox = ( 3 * vlox / ( 4*pi ) )^(1/3); %[m] hypothetical radius of a spherical tank
+R_sphere_rp1 = ( 3 * vrp1 / ( 4*pi ) )^(1/3); %[m] hypothetical radius of a spherical tank
+
+if R_sphere_lox < 0.98 * diam/2
+    y_lox = @(t) 2*R_sphere_lox;%fluid level inside the tank [m]
+    l_lox = @(t) y_lox(t) + 2*t;%height of the tank [m]
+    S_lox = @(t) 4 * pi * R_sphere_lox^2;%surface of tank [m^2]
+else 
+    V_cyl_lox = @(t) vlox - V_obl(t); %volume of the cylindrical part [m^3]
+    h_cyl_lox = @(t) V_cyl_lox(t) / (pi*R_int(t)^2); %height of cylindrical part [m]
+    y_lox = @(t) h_cyl_lox(t) + 2*R_int(t)/AR;%fluid level inside the tank [m]
+    l_lox = @(t) y_lox(t) + 2*t;%height of the tank [m]
+    S_cyl_lox = @(t) 2*pi*R_int(t)* h_cyl_lox(t); %surface of cylindrical part [m^2]
+    S_obl_lox = @(t) 2*pi * R_int(t)^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
+    S_lox = @(t) S_obl_lox(t) + S_cyl_lox(t); %surface of the lox tank [m^2]
+end
+
+if R_sphere_rp1 < 0.98 * diam/2
+    y_rp1 = @(t) 2*R_sphere_rp1;%fluid level inside the tank [m]
+    l_rp1 = @(t) y_rp1(t) + 2*t;%height of the tank [m]
+    S_rp1 = @(t) 4 * pi * R_sphere_rp1^2;%surface of tank [m^2]
+else
+    V_cyl_rp1 = @(t) vrp1 - V_obl(t); %volume of the cylindrical part [m^3]
+    h_cyl_rp1 = @(t) V_cyl_rp1(t) / (pi*R_int(t)^2); %height of cylindrical part [m]
+    y_rp1 = @(t) h_cyl_rp1(t) + 2*R_int(t)/AR;%fluid level inside the tank [m]
+    l_rp1 = @(t) y_rp1(t) + 2*t;%height of the tank [m]
+    S_cyl_rp1 = @(t) 2*pi*R_int(t)* h_cyl_rp1(t); %surface of cylindrical part [m^2]
+    S_obl_rp1 = @(t) 2*pi * R_int(t)^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
+    S_rp1 = @(t) S_obl_rp1(t) + S_cyl_rp1(t); %surface of the rp1 tank [m^2]
+end
+
 
 %pressure at base with longitudinal acceleration (Stevin's law)
-p_lox = @(t) p + l_lox(t) * rholox * acc; %[Pa] pressure at bottom of tank during acceleration
-p_rp1 = @(t) p + l_rp1(t) * rhorp1 * acc; %[Pa] pressure at bottom of tank during acceleration
+p_lox = @(t) p + y_lox(t) * rholox * acc; %[Pa] pressure at bottom of tank during acceleration
+p_rp1 = @(t) p + y_rp1(t) * rhorp1 * acc; %[Pa] pressure at bottom of tank during acceleration
 
 %thickness of tanks
 f_lox = @(t) t - (diam - 2*t)*p_lox(t)/( 2*sy );
@@ -212,16 +239,12 @@ h.tank_rp1 = l_rp1(t_rp1); %[m] height of tank
 h.tot = h.tank_lox + h.tank_rp1; %[m] total height of tanks together
 
 %surfaces estimation
-S_cyl_lox = 2*pi*R_int(t_lox)* h_cyl_lox(t_lox); %surface of cylindrical part [m^2]
-S_cyl_rp1 = 2*pi*R_int(t_rp1)* h_cyl_rp1(t_rp1); %surface of cylindrical part [m^2]
-S_obl_lox = 2*pi * R_int(t_lox)^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
-S_obl_rp1 = 2*pi * R_int(t_rp1)^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
-S_lox = S_obl_lox + S_cyl_lox; %surface of the lox tank [m^2]
-S_rp1 = S_obl_rp1 + S_cyl_rp1; %surface of the rp1 tank [m^2]
+Slox = S_lox(t_lox);%surface of the lox tank [m^2]
+Srp1 = S_rp1(t_rp1);%surface of the rp1 tank [m^2]
 
 %mass estimation
-M.tank_lox = rho * S_lox * t_lox; %mass of the empty lox tank [kg]
-M.tank_rp1 = rho * S_rp1 * t_rp1; %mass of the empty rp1 tank [kg]
+M.tank_lox = rho * Slox * t_lox; %mass of the empty lox tank [kg]
+M.tank_rp1 = rho * Srp1 * t_rp1; %mass of the empty rp1 tank [kg]
 M.tot_lox = M.tank_lox + mlox; %[kg] mass of full lox tank
 M.tot_rp1 = M.tank_rp1 + mrp1; %[kg] mass of full rp1 tank
 M.tot = M.tot_lox + M.tot_rp1; %[kg] total mass of tanks and propellant
