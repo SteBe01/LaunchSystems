f5b346a3921080e303cbb17c33fec49e906c2d9d
PietroBolsi <pitero.bolsi@gmail.com>
2024-11-13 19:06:38 +0100

    fixed problems

--- Development/STR/test_iteration.m
+++ Development/STR/test_iteration.m
@@ -1,64 +1,94 @@
 
-%% Tanks mass estimation
+%% iterative method to verify mass estimation and to assess optimal staging
+
 clc;
 clear;
 close all;
 
-Is = [311; 311]; %[s] stages Is (electron - rutherford motor)
-eps = [0.07; 0.12]; %[-] stages structural mass indexes
+%data from other departments:
+Is = [311; 311]; %[s] stages Is
 dv = 8.5; %[m/s] required dv;
 m_pay = 250; %[kg] payload mass
-m_fairing = 107; %[kg] fairing mass
-m_motors1 = 315; %[kg] only motor, pumps and batteries (electron - rutherford motor) %pump-fed
-m_motors2 = 45; %[kg] only motor, pumps and batteries (electron - rutherford motor) %pump-fed
-
-%GLOM from baseline
-[GLOM.m_stag, GLOM.m_tot, GLOM.m_prop] = TANDEM(Is, eps, dv, m_pay, 0);
-
-%stage 1 analysis
+M.other1 = 4000; %[kg] m_fairing+m_pay+m_connectors+m_motors+m_avionics+m_cables+etc (basically anything that is neither propellant or tanks in first stack)
+M.other2 = 1500; %[kg] m_fairing+m_pay+m_connectors+m_motors+m_avionics+m_cables+etc (basically anything that is neither propellant or tanks in second stack)
 OF = 2.58; %[-] Ox/Fu ratio for LOX-RP1
-M1.rp1 = GLOM.m_prop(1) * 1 / (1+OF); %[kg] mass of rp1
-M1.lox = GLOM.m_prop(1) * OF / (1+OF);%[kg] mass of lox
-M1.prop = M1.lox + M1.rp1;%[kg] mass of propellant
-M1.rhorp1 = 807;  %[kg/m^3] density of rp1
-M1.rholox = 1140; %[kg/m^3] density of lox
-M1.motor = m_motors1;
-
-n = 10; %[-] load factor
+n = 5; %[-] load factor
 d = 1.2; %[m] external diameter
 AR = 2; %aspect ratio of oblate domes [-]
 loads.acc = n*9.81; %longitudinal acceleration [m/s^2]
-mat1 = 1; % 1 for Ti, 2 for Al, 3 for Steel, 4 for Carbon Fiber
+%stage 1
+M1.motor = 315; %[kg] only motor, pumps and batteries (electron - rutherford motor) %pump-fed
+M1.rhorp1 = 807;  %[kg/m^3] density of rp1
+M1.rholox = 1140; %[kg/m^3] density of lox
+mat1 = 1; % 1 for Ti, 2 for Al, 3 for Steel, 4 for Carbon Fiber %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% in future versions can be optimized the material selection in function
 press1 = 2; % 0 for unpressurized, 1 for pressure-fed, 2 for pump-fed, 3 for blowdown
-
-%find S1 info
-[M1, h1, th1] = inert_mass(M1, d, AR, loads, mat1, press1);
-
-%stage 2 analysis
-OF = 2.58; %[-] Ox/Fu ratio for LOX-RP1
-M2.rp1 = GLOM.m_prop(2) * 1 / (1+OF); %[kg] mass of rp1
-M2.lox = GLOM.m_prop(2) * OF / (1+OF);%[kg] mass of lox
-M2.prop = M2.lox + M2.rp1;%[kg] mass of propellant
+%stage 2
+M2.motor = 45; %[kg] only motor, pumps and batteries (electron - rutherford motor) %pump-fed
 M2.rhorp1 = 807;  %[kg/m^3] density of rp1
 M2.rholox = 1140; %[kg/m^3] density of lox
-mat2 = 1; % 1 for Ti, 2 for Al, 3 for Steel, 4 for Carbon Fiber
-press2 = 1; % 0 for unpressurized, 1 for pressure-fed, 2 for pump-fed, 3 for blowdown
-M2.motor = m_motors2;
+mat2 = 1; % 1 for Ti, 2 for Al, 3 for Steel, 4 for Carbon Fiber %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% in future versions can be optimized the material selection in function
+press2 = 2; % 0 for unpressurized, 1 for pressure-fed, 2 for pump-fed, 3 for blowdown
 
-%find S2 info
-[M2, h2, th2] = inert_mass(M2, d, AR, loads, mat2, press2);
+%first guesses:
+eps0 = [0.1; 0.1]; %[-] stages structural mass indexes
+[GLOM.m_stag0, GLOM.m_tot0, GLOM.m_prop0] = TANDEM(Is, eps0, dv, m_pay, 0);
+
+%while loop parameters:
+eps_new = eps0;
+i = 2;
+Nmax = 1000;
+err = 1;
+tol = 1e-8;
+eps_real = zeros(2, Nmax);
+eps_real(:,1) = eps0;
+r = 0.0;
+
+while i < Nmax && err > tol
+    eps_old = eps_new;
+
+    %compute new values:
+    [GLOM.m_stag, GLOM.m_tot, GLOM.m_prop] = TANDEM(Is, eps_old, dv, m_pay, 0);
+    
+    %first stage
+    M1.rp1 = GLOM.m_prop(1) * 1 / (1+OF); %[kg] mass of rp1
+    M1.lox = GLOM.m_prop(1) * OF / (1+OF);%[kg] mass of lox
+
+    %second stage
+    M2.rp1 = GLOM.m_prop(2) * 1 / (1+OF); %[kg] mass of rp1
+    M2.lox = GLOM.m_prop(2) * OF / (1+OF);%[kg] mass of lox
+
+    %tank masses estimation
+    % [M1, h1, th1] = tank_mass(M1, d, AR, loads, mat1, press1);
+    % [M2, h2, th2] = tank_mass(M2, d, AR, loads, mat2, press2);
+    [M1, h1, th1] = inert_mass(M1, d, AR, loads, mat1, press1);
+    [M2, h2, th2] = inert_mass(M2, d, AR, loads, mat2, press2);
+
+    %real parameters:
+    % eps1 = real( ( M1.tot + M.other1 - M.other2 - M1.prop ) / ( M1.tot + M.other1 - M.other2 ) );
+    % eps2 = real( ( M2.tot + M.other2 - M2.prop ) / ( M2.tot + M.other2 ) );
+    eps1 = M1.inert / M1.tot;
+    eps2 = M2.inert / M2.tot;
+
+    eps_real(:, i) = [eps1; eps2];
+
+    err = norm(eps_real(:, i)-eps_old);
+    eps_new = eps_old*r + eps_real(:, i)*(1-r);%(eps_old + eps_real(:, i))/2;%eps_old - k*[0.001; 0.001] ;
+    i = i+1;
+end
+
+%total propellants
+M1.prop = M1.lox + M1.rp1;%[kg] mass of propellant
+M2.prop = M2.lox + M2.rp1;%[kg] mass of propellant
+
+M.M0 = M1.tot + M2.tot + M.other1;%[kg] initial mass (first stack mass)
+M.M1 = M.M0 - GLOM.m_prop(1) - M.other1 + M.other2; %[kg] second stack mass
 
 %MA parameters
-M.M0 = M1.tot + M2.tot + m_pay + m_fairing;%[kg] initial mass (first stack mass)
 M.M0e = M.M0 - M1.prop; %[kg] mass at S1 ending
 M.MR1 = M.M0 / M.M0e; 
-M.M1 = M2.tot + m_pay + m_fairing;%[kg] second stack mass
 M.M1e = M.M1 - M2.prop; %[kg] mass at S2 ending
 M.MR2 = M.M1 / M.M1e;
 
-M.eps1 = M1.inert / M1.tot; %[-] first stage structural mass index
-M.eps2 = M2.inert / M2.tot; %[-] first stage structural mass index
-% M.eps2 = ( M2.inert + m_pay + m_fairing ) / ( M2.tot + m_pay + m_fairing); %[-] first stage structural mass index
 %% Functions
 
 function [m_stag, m_tot, m_prop] = TANDEM(Is, e, dv, m_pay, fsolveOut)
@@ -96,17 +126,12 @@ m_stag = zeros(n, 1);   %initialize
 m_stag(n) = (m(n)-1)/(1-e(n)*m(n))*m_pay;
 
 if n == 1
-
     m_stag = m_stag(1);
-
 else
     i = n-1;
     while i >= 1
-
         m_stag(i) = (m(i)-1)/(1-e(i)*m(i))*(m_pay+sum(m_stag));
-
         i = i-1;
-
     end
 end
 
@@ -125,6 +150,7 @@ end
 % %propellant masses
 % mlox = M.lox; %[kg]
 % mrp1 = M.rp1; %[kg]
+% M.prop = mrp1 + mlox; %[kg] total propellant mass
 % 
 % %propellant densities
 % rholox = M.rholox; %[kg/m^3]
@@ -143,7 +169,7 @@ end
 %     case 1 %case for pressure-fed
 %         MEOP = 2.8 * 1e6; %[Pa] internal tank pressure
 %     case 2 %case for pump-fed
-%         MEOP = 700 * 1e3; %[Pa] internal tank pressure
+%         MEOP = 300 * 1e3; %[Pa] internal tank pressure
 %     case 3 % case for blowdown
 %         MEOP = 50 * 1e6; %[Pa] internal tank pressure
 % end
@@ -182,54 +208,54 @@ end
 % jproof = 1.25;
 % jburst = 1.5;
 % p = MDP * jproof;
+% p = MEOP;
+% 
 % 
 % %tanks shape definition
 % e = sqrt( 1 - 1 / AR^2 );         %eccentricity of oblate part [-]
-% R_int = @(t) (diam - 2*t) / 2;    %internal radius of the tank [m]
-% V_obl = @(t) (4/3)*pi*R_int(t)^3 / AR; %volume of the two oblate parts [m^3]
+% R_int = diam / 2;    %internal radius of the tank [m]
+% V_obl = (4/3)*pi*R_int^3 / AR; %volume of the two oblate parts [m^3]
 % 
 % %spherical tank hypothesis
 % R_sphere_lox = ( 3 * vlox / ( 4*pi ) )^(1/3); %[m] hypothetical radius of a spherical tank
 % R_sphere_rp1 = ( 3 * vrp1 / ( 4*pi ) )^(1/3); %[m] hypothetical radius of a spherical tank
 % 
 % if R_sphere_lox < 0.98 * diam/2
-%     y_lox = @(t) 2*R_sphere_lox;%fluid level inside the tank [m]
-%     l_lox = @(t) y_lox(t) + 2*t;%height of the tank [m]
-%     S_lox = @(t) 4 * pi * R_sphere_lox^2;%surface of tank [m^2]
+%     y_lox = 2*R_sphere_lox;%fluid level inside the tank [m]
+%     l_lox = @(t) y_lox + 2*t;%height of the tank [m]
+%     S_lox = 4 * pi * R_sphere_lox^2;%surface of tank [m^2]
 % else 
-%     V_cyl_lox = @(t) vlox - V_obl(t); %volume of the cylindrical part [m^3]
-%     h_cyl_lox = @(t) V_cyl_lox(t) / (pi*R_int(t)^2); %height of cylindrical part [m]
-%     y_lox = @(t) h_cyl_lox(t) + 2*R_int(t)/AR;%fluid level inside the tank [m]
-%     l_lox = @(t) y_lox(t) + 2*t;%height of the tank [m]
-%     S_cyl_lox = @(t) 2*pi*R_int(t)* h_cyl_lox(t); %surface of cylindrical part [m^2]
-%     S_obl_lox = @(t) 2*pi * R_int(t)^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
-%     S_lox = @(t) S_obl_lox(t) + S_cyl_lox(t); %surface of the lox tank [m^2]
+%     V_cyl_lox = vlox - V_obl; %volume of the cylindrical part [m^3]
+%     h_cyl_lox = V_cyl_lox / (pi*R_int^2); %height of cylindrical part [m]
+%     y_lox = h_cyl_lox + 2*R_int/AR;%fluid level inside the tank [m]
+%     l_lox = @(t) y_lox + 2*t;%height of the tank [m]
+%     S_cyl_lox = 2*pi*R_int* h_cyl_lox; %surface of cylindrical part [m^2]
+%     S_obl_lox = 2*pi * R_int^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
+%     S_lox = S_obl_lox + S_cyl_lox; %surface of the lox tank [m^2]
 % end
 % 
 % if R_sphere_rp1 < 0.98 * diam/2
-%     y_rp1 = @(t) 2*R_sphere_rp1;%fluid level inside the tank [m]
-%     l_rp1 = @(t) y_rp1(t) + 2*t;%height of the tank [m]
-%     S_rp1 = @(t) 4 * pi * R_sphere_rp1^2;%surface of tank [m^2]
+%     y_rp1 = 2*R_sphere_rp1;%fluid level inside the tank [m]
+%     l_rp1 = @(t) y_rp1 + 2*t;%height of the tank [m]
+%     S_rp1 = 4 * pi * R_sphere_rp1^2;%surface of tank [m^2]
 % else
-%     V_cyl_rp1 = @(t) vrp1 - V_obl(t); %volume of the cylindrical part [m^3]
-%     h_cyl_rp1 = @(t) V_cyl_rp1(t) / (pi*R_int(t)^2); %height of cylindrical part [m]
-%     y_rp1 = @(t) h_cyl_rp1(t) + 2*R_int(t)/AR;%fluid level inside the tank [m]
-%     l_rp1 = @(t) y_rp1(t) + 2*t;%height of the tank [m]
-%     S_cyl_rp1 = @(t) 2*pi*R_int(t)* h_cyl_rp1(t); %surface of cylindrical part [m^2]
-%     S_obl_rp1 = @(t) 2*pi * R_int(t)^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
-%     S_rp1 = @(t) S_obl_rp1(t) + S_cyl_rp1(t); %surface of the rp1 tank [m^2]
+%     V_cyl_rp1 = vrp1 - V_obl; %volume of the cylindrical part [m^3]
+%     h_cyl_rp1 = V_cyl_rp1 / (pi*R_int^2); %height of cylindrical part [m]
+%     y_rp1 = h_cyl_rp1 + 2*R_int/AR;%fluid level inside the tank [m]
+%     l_rp1 = @(t) y_rp1 + 2*t;%height of the tank [m]
+%     S_cyl_rp1 = 2*pi*R_int* h_cyl_rp1; %surface of cylindrical part [m^2]
+%     S_obl_rp1 = 2*pi * R_int^2 * ( 1 + (1/(e*AR^2)) * atanh( e ) ); %surface of the oblate parts together [m^2]
+%     S_rp1 = S_obl_rp1 + S_cyl_rp1; %surface of the rp1 tank [m^2]
 % end
 % 
 % 
 % %pressure at base with longitudinal acceleration (Stevin's law)
-% p_lox = @(t) p + y_lox(t) * rholox * acc; %[Pa] pressure at bottom of tank during acceleration
-% p_rp1 = @(t) p + y_rp1(t) * rhorp1 * acc; %[Pa] pressure at bottom of tank during acceleration
+% p_lox = p + y_lox * rholox * acc; %[Pa] pressure at bottom of tank during acceleration
+% p_rp1 = p + y_rp1 * rhorp1 * acc; %[Pa] pressure at bottom of tank during acceleration
 % 
 % %thickness of tanks
-% f_lox = @(t) t - (diam - 2*t)*p_lox(t)/( 2*sy );
-% f_rp1 = @(t) t - (diam - 2*t)*p_rp1(t)/( 2*sy );
-% t_lox = fzero(f_lox, 1e-3); %[m] lox tank thickness
-% t_rp1 = fzero(f_rp1, 1e-3); %[m] rp1 tank thickness
+% t_lox = diam*p_lox/( 2*sy ); %[m] lox tank thickness
+% t_rp1 = diam*p_rp1/( 2*sy ); %[m] rp1 tank thickness
 % t.lox = t_lox;
 % t.rp1 = t_rp1;
 % 
@@ -245,13 +271,9 @@ end
 % h.tank_rp1 = l_rp1(t_rp1); %[m] height of tank
 % h.tot = h.tank_lox + h.tank_rp1; %[m] total height of tanks together
 % 
-% %surfaces estimation
-% Slox = S_lox(t_lox);%surface of the lox tank [m^2]
-% Srp1 = S_rp1(t_rp1);%surface of the rp1 tank [m^2]
-% 
 % %mass estimation
-% M.tank_lox = rho * Slox * t_lox; %mass of the empty lox tank [kg]
-% M.tank_rp1 = rho * Srp1 * t_rp1; %mass of the empty rp1 tank [kg]
+% M.tank_lox = rho * S_lox * t_lox; %mass of the empty lox tank [kg]
+% M.tank_rp1 = rho * S_rp1 * t_rp1; %mass of the empty rp1 tank [kg]
 % M.tot_lox = M.tank_lox + mlox; %[kg] mass of full lox tank
 % M.tot_rp1 = M.tank_rp1 + mrp1; %[kg] mass of full rp1 tank
 % M.tot = M.tot_lox + M.tot_rp1; %[kg] total mass of tanks and propellant
@@ -398,5 +420,6 @@ M.tot = M.tot_lox + M.tot_rp1 + M.motor; %[kg] total mass of motors, tanks and p
 M.tanks = M.tank_lox + M.tank_rp1; %[kg] mass of the two empty tanks
 M.inert = M.tanks + M.motor;%[kg] inert mass of stage (motors and tanks)
 end
+
 
 
