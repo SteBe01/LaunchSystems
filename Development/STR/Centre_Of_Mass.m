16de5dec154c09da952dbeb3b45235e2e5e45905
Leo777Deo <goldenexp.requiem@gmail.com>
2024-11-22 14:29:35 +0100

    Update Centre_Of_Mass.m

--- Development/STR/Centre_Of_Mass.m
+++ Development/STR/Centre_Of_Mass.m
@@ -1,4 +1,10 @@
 function [COM,MoI] = Centre_Of_Mass(TR,Tank1,Tank2,Engine1,Engine2,t)
+function [COM,MoI,TR] = Centre_Of_Mass(TR,Tank1,Tank2,Tank3,Engine1,Engine2,Engine3,t)
+
+switch TR.N_stages
+
+    case 2
+
 
 %% Main outputs:
 
@@ -19,9 +25,6 @@ function [COM,MoI] = Centre_Of_Mass(TR,Tank1,Tank2,Engine1,Engine2,t)
 
 % MoI.Stk2.TR.Jy : value of Jy at t = t2 (>tb1) 
 % MoI.Stk2.TR.Jy_tb1 : value of Jy at t = tb1 (detachment) 
-
-
-%% 
 
 % structures to be defined for Engine and t
 % t.t1 = 0;
@@ -73,14 +76,10 @@ M_cables = 1.058*(TR.Length^(0.25))*sqrt(TR.M.M01); % [kg] Empirical formula sli
 
 M_avionics = 10*(TR.M.M01)^(0.361); % [kg] Empirical formula slides Maggi 06, structures part 1
 
-M_cables_distributed = M_cables/19;
+M_struct1 = (2.55*(10^-4))*Engine1.T;
+M_struct2 = (2.55*(10^-4))*Engine2.T;
 
-% To distinguish between 1st part of flight and 2nd part after stage 1 is
-% jettisoned
-
-if t.t1 <t.t_burn1
-
-%% STACK 1: t = [0-t1]
+M_cables_distributed = M_cables/TR.Length;
 
 Ms1_stage_ratio = 1- TR.M.Ms2/TR.M.Ms1;
 
@@ -90,18 +89,22 @@ Ms2_stage_ratio = TR.M.Ms2/TR.M.Ms1;
 
 M_avionics_s2 = M_avionics*Ms2_stage_ratio;
 
+
+% To distinguish between 1st part of flight and 2nd part after stage 1 is
+% jettisoned
+
+if t.t1 <t.t_burn1
+
+%% STACK 1: t = [0-t1]
+
 t1 = t.t1;
 t2 = 0; % tandem launcher, so no prop of 2nd satge is burned
 
 % x = 0 at the tip
 
-% First of all of the lengths are defined as in slide 10 of str mass sildes
-% Percentage of 5% added to L fair and 10% to interstage to account for
-% space to place parachute
-
 % 2 stage
 
-x_fair = TR.Fair.fair_length*(1+0.05);
+x_fair = TR.Fair.fair_length;
 
 if strcmp(Tank2.OX.FLAG, 'Cyl') && strcmp(Tank2.FU.FLAG, 'Cyl')
 % Top FU, bottom OX
@@ -137,6 +140,16 @@ end
 L_interstage = (5/4)*Diam_1*(1+0.1);%L_nozzle2*(1+0.1);
 
 x_interstage = x_dwrd_skirt_s2 + L_interstage;
+
+N_parts_s2 = 10; % 9 + half interstage
+N_parts_s1 = 10; % 9 + half interstage
+
+M_cables_s2_tot = M_cables_distributed*(x_dwrd_skirt_s2 + (L_interstage/2));
+M_cables_s1_tot = M_cables_distributed*(TR.Length-(x_dwrd_skirt_s2 + (L_interstage/2)));
+
+M_cables_s1 = M_cables_s1_tot/N_parts_s1;
+M_cables_s2 = M_cables_s2_tot/N_parts_s2;
+
 
 % 1 stage
 
@@ -270,13 +283,13 @@ COM.Stk1.Fins.x_fins = x_cyl_11 + (L_end*(3/4)); % x coord of fins
 
 COM.Stk1.Pay.m_pay = 250;
 
-COM.Stk1.Fair.m_fair = TR.Fair.M_fair +M_cables_distributed;
+COM.Stk1.Fair.m_fair = TR.Fair.M_fair +M_cables_s2;
 
-COM.Stk1.Prac2.m_parachute_2 = 0.1*TR.M.Ms2+ M_cables_distributed + M_avionics_s2/2;
+COM.Stk1.Prac2.m_parachute_2 = 0.1*TR.M.Ms2+ M_cables_s2 + M_avionics_s2/2 +  M_struct2/2;
 
-COM.Stk1.Tank2_FU.m_tank2_FU = Tank2.FU.M_tot_tank_fuel+ M_cables_distributed;
+COM.Stk1.Tank2_FU.m_tank2_FU = Tank2.FU.M_tot_tank_fuel+ M_cables_s2;
 
-COM.Stk1.Tank2_OX.m_tank2_OX = Tank2.OX.M_tot_tank_lox+ M_cables_distributed;
+COM.Stk1.Tank2_OX.m_tank2_OX = Tank2.OX.M_tot_tank_lox+ M_cables_s2;
 
 COM.Stk1.Prop2_FU.m_prop2_FU0 = Tank2.FU.M_fuel;
 COM.Stk1.Prop2_FU.m_prop2_FU = COM.Stk1.Prop2_FU.m_prop2_FU0 - (m_dotfuel_2*t2);
@@ -284,17 +297,17 @@ COM.Stk1.Prop2_FU.m_prop2_FU = COM.Stk1.Prop2_FU.m_prop2_FU0 - (m_dotfuel_2*t2);
 COM.Stk1.Prop2_OX.m_prop2_OX0 = Tank2.OX.M_lox;
 COM.Stk1.Prop2_OX.m_prop2_OX = COM.Stk1.Prop2_OX.m_prop2_OX0 - (m_dotlox_2*t2);
 
-COM.Stk1.Pipes2.m_pipes2 = 10+ M_cables_distributed;
+COM.Stk1.Pipes2.m_pipes2 = 10+ M_cables_s2;
 
-COM.Stk1.Aft_Struct2.m_aft_struct2 = 100+ M_cables_distributed + M_avionics_s2/2;
+COM.Stk1.Aft_Struct2.m_aft_struct2 = M_struct2/2+ M_cables_s2+ M_avionics_s2/2;
 
-COM.Stk1.Engine2.m_engine2 = 300+ M_cables_distributed;
+COM.Stk1.Engine2.m_engine2 = Engine2.M+ M_cables_s2;
 
-COM.Stk1.Interstage.m_interstage = 0.1*TR.M.Ms1 + 20+ M_cables_distributed + M_avionics_s1/2;
+COM.Stk1.Interstage.m_interstage = 0.1*TR.M.Ms1 + (M_struct1+M_struct2)/2+ M_cables_s1+M_cables_s2+ M_avionics_s1/2;
 
-COM.Stk1.Tank1_FU.m_tank1_FU = Tank1.FU.M_tot_tank_fuel+ M_cables_distributed;
+COM.Stk1.Tank1_FU.m_tank1_FU = Tank1.FU.M_tot_tank_fuel+ M_cables_s1;
 
-COM.Stk1.Tank1_OX.m_tank1_OX = Tank1.OX.M_tot_tank_lox+ M_cables_distributed;
+COM.Stk1.Tank1_OX.m_tank1_OX = Tank1.OX.M_tot_tank_lox+ M_cables_s1;
 
 COM.Stk1.Prop1_FU.m_prop1_FU0 = Tank1.FU.M_fuel;
 COM.Stk1.Prop1_FU.m_prop1_FU = COM.Stk1.Prop1_FU.m_prop1_FU0 - ((m_dotfuel_1*t1));
@@ -302,13 +315,13 @@ COM.Stk1.Prop1_FU.m_prop1_FU = COM.Stk1.Prop1_FU.m_prop1_FU0 - ((m_dotfuel_1*t1)
 COM.Stk1.Prop1_OX.m_prop1_OX0 = Tank1.OX.M_lox;
 COM.Stk1.Prop1_OX.m_prop1_OX = COM.Stk1.Prop1_OX.m_prop1_OX0 - ((m_dotlox_1*t1));
 
-COM.Stk1.Pipes1.m_pipes1 = 10+ M_cables_distributed;
+COM.Stk1.Pipes1.m_pipes1 = 10 + M_cables_s1;
 
-COM.Stk1.Aft_Struct1.m_aft_struct1 = 300+ M_cables_distributed +M_avionics_s1;
+COM.Stk1.Aft_Struct1.m_aft_struct1 = M_struct1+ M_cables_s1 +M_avionics_s1;
 
-COM.Stk1.Engine1.m_engine1 = 200+ M_cables_distributed; % Engine + nozzle
+COM.Stk1.Engine1.m_engine1 = Engine1.M + M_cables_s1; % Engine + nozzle
 
-COM.Stk1.Fins.m_fins = 10+ M_cables_distributed; % x coord of fins
+COM.Stk1.Fins.m_fins = 10 + M_cables_s1; % x coord of fins
 
 %% Computation of position of COM wrt nose and M0
 
@@ -375,6 +388,14 @@ COM.Stk1.TR.M_LV0 = (sum(COM_vec_m0));% [kg] t = 0
 
 COM.Stk1.TR.M_prop = COM_vec_m(4,1) +COM_vec_m(6,1)+COM_vec_m(12,1) + COM_vec_m(15,1); % [kg] t = t1 + t2
 COM.Stk1.TR.M_prop0 = COM.Stk1.Prop2_FU.m_prop2_FU0 + COM.Stk1.Prop2_OX.m_prop2_OX0 + COM.Stk1.Prop1_FU.m_prop1_FU0 +COM.Stk1.Prop1_OX.m_prop1_OX0;% [kg] t = 0
+
+COM_m_vec_s1 = [COM.Stk1.Interstage.m_interstage;COM.Stk1.Tank1_FU.m_tank1_FU;COM.Stk1.Pipes1.m_pipes1;COM.Stk1.Tank1_OX.m_tank1_OX;COM.Stk1.Aft_Struct1.m_aft_struct1;COM.Stk1.Engine1.m_engine1;COM.Stk1.Fins.m_fins];
+
+TR.M_s1_real = sum(COM_m_vec_s1);
+
+COM_m_vec_s2 = [COM.Stk1.Fair.m_fair;COM.Stk1.Prac2.m_parachute_2;COM.Stk1.Tank2_FU.m_tank2_FU;COM.Stk1.Tank2_OX.m_tank2_OX;COM.Stk1.Pipes2.m_pipes2;COM.Stk1.Aft_Struct2.m_aft_struct2;COM.Stk1.Engine2.m_engine2];
+
+TR.M_s2_real = sum(COM_m_vec_s2);
 
 %% Moments of Inertia
 
@@ -544,10 +565,6 @@ MoI.Stk1.TR.Jy0 = MoI_vec_mxquad0+ sum(MoI_vec_J0); % [kg m^2] t = 0
 
 elseif t.t1>=t.t_burn1 
 
-Ms2_stage_ratio = TR.M.Ms2/TR.M.Ms1;
-
-M_avionics_s2 = M_avionics*Ms2_stage_ratio;
-M_cables_s2 = M_cables*Ms2_stage_ratio;
 
 t2 = t.t2;
 t1 = t.t_burn1;
@@ -584,7 +601,14 @@ x_dwrd_skirt_s2 = x_sph_21 + L_dwrd_skirt_s2;
 
 end
 
-x_end_2 = x_dwrd_skirt_s2 + L_nozzle2;  % new end of LV, 2^nd stack end
+COM.Stk2.x_end_2 = x_dwrd_skirt_s2 + L_nozzle2;  % new end of LV, 2^nd stack end
+
+N_parts_s2 = 10; % 9 + half interstage
+
+M_cables_s2_tot = M_cables_distributed*(COM.Stk2.x_end_2);
+
+M_cables_s2 = M_cables_s2_tot/N_parts_s2;
+
 
 %% C.O.M. positioning for each component
 
@@ -634,13 +658,13 @@ end
 
 COM.Stk2.Pay.m_pay = 250;
 
-COM.Stk2.Fair.m_fair = TR.Fair.M_fair+ M_cables_distributed;
+COM.Stk2.Fair.m_fair = TR.Fair.M_fair+ M_cables_s2;
 
-COM.Stk2.Prac2.m_parachute_2 = 0.1*TR.M.Ms2+ M_cables_distributed + M_avionics_s2/2;
+COM.Stk2.Prac2.m_parachute_2 = 0.1*TR.M.Ms2+ M_cables_s2 + M_avionics_s2/2+ M_struct2/2;
 
-COM.Stk2.Tank2_FU.m_tank2_FU = Tank2.FU.M_tot_tank_fuel+ M_cables_distributed;
+COM.Stk2.Tank2_FU.m_tank2_FU = Tank2.FU.M_tot_tank_fuel+ M_cables_s2;
 
-COM.Stk2.Tank2_OX.m_tank2_OX = Tank2.OX.M_tot_tank_lox+ M_cables_distributed;
+COM.Stk2.Tank2_OX.m_tank2_OX = Tank2.OX.M_tot_tank_lox+ M_cables_s2;
 
 COM.Stk2.Prop2_FU.m_prop2_FU0 = Tank2.FU.M_fuel;
 COM.Stk2.Prop2_FU.m_prop2_FU = COM.Stk2.Prop2_FU.m_prop2_FU0 - (m_dotfuel_2*t2);
@@ -648,11 +672,11 @@ COM.Stk2.Prop2_FU.m_prop2_FU = COM.Stk2.Prop2_FU.m_prop2_FU0 - (m_dotfuel_2*t2);
 COM.Stk2.Prop2_OX.m_prop2_OX0 = Tank2.OX.M_lox;
 COM.Stk2.Prop2_OX.m_prop2_OX = COM.Stk2.Prop2_OX.m_prop2_OX0 - (m_dotlox_2*t2);
 
-COM.Stk2.Pipes2.m_pipes2 = 10+ M_cables_distributed;
+COM.Stk2.Pipes2.m_pipes2 = 10 + M_cables_distributed;
 
-COM.Stk2.Aft_Struct2.m_aft_struct2 = 100+ M_cables_distributed+ M_avionics_s2/2;
+COM.Stk2.Aft_Struct2.m_aft_struct2 = M_struct2/2+ M_cables_s2+ M_avionics_s2/2;
 
-COM.Stk2.Engine2.m_engine2 = 300+ M_cables_distributed;
+COM.Stk2.Engine2.m_engine2 = Engine2.M+ M_cables_s2;
 
 %% Computation of position of COM wrt nose and M_tb1 (at t = tb1)
 
@@ -698,6 +722,10 @@ COM.Stk2.TR.M_LV_tb1 = (sum(COM_vec_m_tb1));% [kg] t = tb1
 
 COM.Stk2.TR.M_prop = COM_vec_m(4,1) +COM_vec_m(6,1); % [kg] t = tb1 + t2
 COM.Stk2.TR.M_prop_tb1 = COM.Stk2.Prop2_FU.m_prop2_FU0 + COM.Stk2.Prop2_OX.m_prop2_OX0;% [kg] t = tb1
+
+COM_m_vec_s2 =[COM.Stk2.Fair.m_fair;COM.Stk2.Prac2.m_parachute_2;COM.Stk2.Prop2_FU.m_prop2_FU;COM.Stk2.Tank2_FU.m_tank2_FU;COM.Stk2.Prop2_OX.m_prop2_OX;COM.Stk2.Tank2_OX.m_tank2_OX;COM.Stk2.Pipes2.m_pipes2;COM.Stk2.Aft_Struct2.m_aft_struct2;COM.Stk2.Engine2.m_engine2];
+
+TR.M_s2_real = sum(COM_m_vec_s2);
 
 %% Moments of Inertia
 
@@ -789,7 +817,183 @@ MoI_vec_mxquad_tb1 = dot(COM_vec_m_tb1,MoI_vec_xquad_tb1);
 
 MoI.Stk2.TR.Jy = MoI_vec_mxquad + sum(MoI_vec_J0); % [kg m^2] t = tb1 + t2
 
-MoI.Stk2.TR.Jy_tb1 = MoI_vec_mxquad_tb1+ sum(MoI_vec_J0); % [kg m^2] t = tb1
+MoI.Stk2.TR.Jy0 = MoI_vec_mxquad_tb1+ sum(MoI_vec_J0); % [kg m^2] t = tb1
 
-end 
+end % if 2 stages
+
+    case 3
+
+L_nozzle2 = Engine2.L_nozzle;
+L_nozzle1 = Engine1.L_nozzle;
+L_nozzle3 = Engine3.L_nozzle;
+
+m_dotlox_2 = Engine2.m_dot_lox;
+m_dotfuel_2 = Engine2.m_dot_fuel;
+m_dotlox_1 = Engine1.m_dot_lox;
+m_dotfuel_1 = Engine1.m_dot_fuel;
+m_dotlox_3 = Engine3.m_dot_lox;
+m_dotfuel_3 = Engine3.m_dot_fuel;
+
+if strcmp(Tank3.OX.FLAG, 'Cyl') && strcmp(Tank3.FU.FLAG, 'Cyl')
+Diam_3 = TR.Diameter;
+
+elseif strcmp(Tank3.OX.FLAG, 'Sphere') && strcmp(Tank3.FU.FLAG, 'Sphere')
+Diam_3 = 2*(max([Tank3.OX.R_sphere_lox;Tank3.FU.R_sphere_fuel]));
+
+end
+
+if strcmp(Tank2.OX.FLAG, 'Cyl') && strcmp(Tank2.FU.FLAG, 'Cyl')
+Diam_2 = TR.Diameter;
+
+elseif strcmp(Tank2.OX.FLAG, 'Sphere') && strcmp(Tank2.FU.FLAG, 'Sphere')
+Diam_2 = 2*(max([Tank2.OX.R_sphere_lox;Tank2.FU.R_sphere_fuel]));
+
+end
+
+if strcmp(Tank1.OX.FLAG, 'Cyl') && strcmp(Tank1.FU.FLAG, 'Cyl')
+Diam_1 = TR.Diameter;
+
+elseif strcmp(Tank1.OX.FLAG, 'Sphere') && strcmp(Tank1.FU.FLAG, 'Sphere')
+Diam_1 = 2*(max([Tank1.OX.R_sphere_lox;Tank1.FU.R_sphere_fuel]));
+
+end
+
+M_cables = 1.058*(TR.Length^(0.25))*sqrt(TR.M.M01); % [kg] Empirical formula slides Maggi 06, structures part 1
+
+M_avionics = 10*(TR.M.M01)^(0.361); % [kg] Empirical formula slides Maggi 06, structures part 1
+
+M_struct1 = (2.55*(10^-4))*Engine1.T;
+M_struct2 = (2.55*(10^-4))*Engine2.T;
+M_struct3 = (2.55*(10^-4))*Engine3.T;
+
+M_cables_distributed = M_cables/TR.Length;
+
+M_s_tot = TR.M.Ms1 + TR.M.Ms2 + TR.M.Ms3;
+
+Ms1_stage_ratio = TR.M.Ms1/M_s_tot;
+
+M_avionics_s1 = M_avionics*Ms1_stage_ratio;
+
+Ms2_stage_ratio =  TR.M.Ms2/M_s_tot;
+
+M_avionics_s2 = M_avionics*Ms2_stage_ratio;
+
+Ms3_stage_ratio =  TR.M.Ms3/M_s_tot;
+
+M_avionics_s3 = M_avionics*Ms3_stage_ratio;
+
+% To distinguish between 1st part of flight and 2nd part after stage 1 is
+% jettisoned and same for t burn 2
+
+
+if t.t1 <t.t_burn1
+
+%% STACK 1: t = [0-t1]
+
+t1 = t.t1;
+t2 = 0; % tandem launcher, so no prop of 2nd satge is burned
+t3 = 0;
+
+% x = 0 at the tip
+
+% 3 stage
+
+x_fair = TR.Fair.fair_length;
+
+if strcmp(Tank2.OX.FLAG, 'Cyl') && strcmp(Tank2.FU.FLAG, 'Cyl')
+% Top FU, bottom OX
+L_fwrd_skirt_s2 = ((1/3)*Diam_2) + Tank2.FU.H_dome;
+L_cyl_tank_FU2 = Tank2.FU.H_cyl;
+L_connection_2 = ((1/4)*Diam_2) + (Tank2.OX.H_dome + Tank2.FU.H_dome);
+L_cyl_tank_OX2 = Tank2.OX.H_cyl;
+L_dwrd_skirt_s2 = ((1/3)*Diam_2) + Tank2.OX.H_dome;
+
+x_fwrd_skirt_s2 = x_fair + L_fwrd_skirt_s2;
+x_cyl_22 = x_fwrd_skirt_s2 + L_cyl_tank_FU2;
+x_connection_2 = x_cyl_22 + L_connection_2;
+x_cyl_21 = x_connection_2 + L_cyl_tank_OX2;
+x_dwrd_skirt_s2 = x_cyl_21 + L_dwrd_skirt_s2;
+
+elseif strcmp(Tank2.OX.FLAG, 'Sphere') && strcmp(Tank2.FU.FLAG, 'Sphere')
+
+   L_fwrd_skirt_s2 = (Tank2.FU.R_sphere_fuel/2)*(1+0.05);
+   L_rem_sphere_FU2 = Tank2.FU.R_sphere_fuel;
+   L_connection_2 = ((1/4)*Diam_2) + ((Tank2.OX.R_sphere_lox/2) + (Tank2.FU.R_sphere_fuel/2));
+   L_rem_sphere_OX2 = Tank2.OX.R_sphere_lox;
+   L_dwrd_skirt_s2 = (Tank2.OX.R_sphere_lox/2) + ((1/3)*Diam_2);
+
+x_fwrd_skirt_s2 = x_fair + L_fwrd_skirt_s2;
+x_sph_22 = x_fwrd_skirt_s2 + L_rem_sphere_FU2;
+x_connection_2 = x_sph_22 + L_connection_2;
+x_sph_21 = x_connection_2 + L_rem_sphere_OX2;
+x_dwrd_skirt_s2 = x_sph_21 + L_dwrd_skirt_s2;
+
+
+end
+
+L_interstage = (5/4)*Diam_1*(1+0.1);%L_nozzle2*(1+0.1);
+
+x_interstage = x_dwrd_skirt_s2 + L_interstage;
+
+N_parts_s2 = 10; % 9 + half interstage
+N_parts_s1 = 10; % 9 + half interstage
+
+M_cables_s2_tot = M_cables_distributed*(x_dwrd_skirt_s2 + (L_interstage/2));
+M_cables_s1_tot = M_cables_distributed*(TR.Length-(x_dwrd_skirt_s2 + (L_interstage/2)));
+
+M_cables_s1 = M_cables_s1_tot/N_parts_s1;
+M_cables_s2 = M_cables_s2_tot/N_parts_s2;
+
+
+% 1 stage
+
+if strcmp(Tank1.OX.FLAG, 'Cyl') && strcmp(Tank1.FU.FLAG, 'Cyl')
+% Top FU, bottom OX
+L_cyl_tank_FU1 = Tank1.FU.H_cyl;
+L_connection_1 = ((1/4)*Diam_1) + (Tank1.OX.H_dome + Tank1.FU.H_dome);
+L_cyl_tank_OX1 = Tank1.OX.H_cyl;
+L_end = Diam_1 + L_nozzle1;
+
+x_cyl_12 = x_interstage + L_cyl_tank_FU1;
+x_connection_1 = x_cyl_12 + L_connection_1;
+x_cyl_11 = L_cyl_tank_OX1 + x_connection_1;
+x_end = x_cyl_11 + L_end;
+
+elseif strcmp(Tank1.OX.FLAG, 'Sphere') && strcmp(Tank1.FU.FLAG, 'Sphere')
+
+   L_rem_sphere_FU1 = (Tank1.FU.R_sphere_fuel);
+   L_connection_1 = ((1/4)*Diam_1) + ((Tank1.OX.R_sphere_lox/2) + (Tank1.FU.R_sphere_fuel/2));
+   L_rem_sphere_OX1 = Tank1.OX.R_sphere_lox;
+   L_end = Diam_1 + L_nozzle1; 
+
+x_sph_12 = x_interstage + L_rem_sphere_FU1;
+x_connection_1 = x_sph_12 + L_connection_1;
+x_sph_11 = L_rem_sphere_OX1 + x_connection_1;
+x_end = x_sph_11 + L_end;
+
+end
+
+Delta_length_err = x_end - TR.Length;
+
+if x_end > TR.Length
+
+  fprintf('Length overbudget: \n Delta_length_err = %d [m] \n ',abs(Delta_length_err));
+
+x_end = TR.Length;
+
+if strcmp(Tank1.OX.FLAG, 'Cyl') && strcmp(Tank1.FU.FLAG, 'Cyl') % correct end length
+
+    L_end = TR.Length - x_cyl_11;
+
+elseif strcmp(Tank1.OX.FLAG, 'Sphere') && strcmp(Tank1.FU.FLAG, 'Sphere')
+    
+L_end = TR.Length - x_sph_11;
+
+end
+
+end
+
+
+
+end % switch
 end
