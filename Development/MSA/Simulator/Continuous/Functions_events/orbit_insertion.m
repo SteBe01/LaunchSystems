65eb0deab9a5201cceeb43ef5949d18ea39faa6c
Pier Francesco Bachini <pier.bachini@gmail.com>
2024-12-05 19:12:23 +0100

    [MSA] Update orbit insertion to correctly rotate velocities

--- Development/MSA/Simulator/Continuous/Functions_events/orbit_insertion.m
+++ Development/MSA/Simulator/Continuous/Functions_events/orbit_insertion.m
@@ -1,7 +1,12 @@
 function [value, isterminal, direction] = orbit_insertion(~, y)
     % value = 1;
     % value = norm(y(1:2)) - (400e3+6378e3);
-    value = y(4);
+
+    beta = atan2(y(2), y(1));
+    ang = pi/2-beta;
+    vec_rotated = [cos(ang) -sin(ang); sin(ang) cos(ang)]*y(3:4);
+
+    value = vec_rotated(2);
     isterminal = 1;
     direction = 0;
 end
