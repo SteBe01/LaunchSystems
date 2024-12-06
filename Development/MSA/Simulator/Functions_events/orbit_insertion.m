41b9f0b81f93d33230c9d3712e868507f4b462a6
SteBe01 <stefano3.belletti@mail.polimi.it>
2024-12-06 22:40:38 +0100

    [MSA] removed old files

--- Development/MSA/Simulator/Functions_events/orbit_insertion.m
+++
@@ -1,5 +0,0 @@
-function [value, isterminal, direction] = orbit_insertion(~, y)
-    value = y(4);
-    isterminal = 1;
-    direction = 0;
-end
--- Development/MSA/Simulator/Functions_events/touchdown.m
+++
@@ -1,5 +0,0 @@
-function [value, isterminal, direction] = touchdown(~, y, params)
-    value = y(2) - params.Re;
-    isterminal = 1;
-    direction = 0;
-end
