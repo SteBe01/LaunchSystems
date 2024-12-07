14a69d387ea59dbcb0b623b357c3b7fccba92525
Leo777Deo <goldenexp.requiem@gmail.com>
2024-12-07 13:52:36 +0100

    Update LOAD_PLOT.m

--- Development/STR/LOAD_PLOT.m
+++ Development/STR/LOAD_PLOT.m
@@ -3,29 +3,29 @@ clear;clc;close all;
 
 %% Trajectory:
 
-stages=load('stages.mat');
-stages = stages.stages;
-
-% q = load("qdyn");
-%q=max(q);
-q=50*10^3;
-FORCES.q = max(q);
-% t_max_Q = find(Max_Q==qdyn);
-% t_max_Q = t_max_Q(1,1);
-
-
-% FORCES.Cl=stages.stg1.Cl;
-% FORCES.Cd = stages.stg1.Cd;
-FORCES.Cl=1.3;
-FORCES.Cd = 1.3;
-
-FORCES.T =stages.stg1.Thrust;
-%FORCES.delta = delta_vec(t_max_Q);
-FORCES.delta =0;
-FORCES.alpha = 0;
-
-FORCES.nx=6;
-FORCES.nz=1;
+% stages=load('stages.mat');
+% stages = stages.stages;
+% 
+% % q = load("qdyn");
+% %q=max(q);
+% q=50*10^3;
+% FORCES.q = max(q);
+% % t_max_Q = find(Max_Q==qdyn);
+% % t_max_Q = t_max_Q(1,1);
+% 
+% 
+% % FORCES.Cl=stages.stg1.Cl;
+% % FORCES.Cd = stages.stg1.Cd;
+% FORCES.Cl=1.3;
+% FORCES.Cd = 1.3;
+% 
+% FORCES.T =stages.stg1.Thrust;
+% %FORCES.delta = delta_vec(t_max_Q);
+% FORCES.delta =0;
+% FORCES.alpha = 0;
+% 
+% FORCES.nx=6;
+% FORCES.nz=1;
 
 %% Script di Pietro: mass_opt_for_variable_dv.m
 h_it=load("h_it.mat");
